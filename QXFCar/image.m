8121771bc73949eab30ed5062df06eaa58a42a99
QXFqianfeng <QXFqianfeng@QXF.local>
2015-12-30 14:58:19 +0800

    第二次提交

---
+++ QXFCar/image.m
@@ -0,0 +1,21 @@
+//
+//  image.m
+//  QXFCar
+//
+//  Created by QXFqianfeng on 15/12/30.
+//  Copyright © 2015年 屈晓飞. All rights reserved.
+//
+
+#import "image.h"
+
+@implementation image
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
